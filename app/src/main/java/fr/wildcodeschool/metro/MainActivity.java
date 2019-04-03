580bc8ae9be2e9988cacf29b40d7269a60d7afd8
Anaclet Biquet <anaclet.biquet@gmail.com>
2019-04-03 11:48:19 +0200

    creat list of stations

--- app/src/main/java/fr/wildcodeschool/metro/MainActivity.java
+++ app/src/main/java/fr/wildcodeschool/metro/MainActivity.java
@@ -1,5 +1,6 @@
 package fr.wildcodeschool.metro;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
@@ -12,41 +13,20 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+
+import static fr.wildcodeschool.metro.Modell.extractStation;
 
 public class MainActivity extends AppCompatActivity {
+    static ArrayList<Station> stations = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String json = null;
-        try {
-            InputStream is = getAssets().open("velos.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        TextView tvHello = findViewById(R.id.textView);
-        try {
-            JSONArray root = new JSONArray(json);
-            for(int i = 0 ; i < root.length() ; i++){
-                JSONObject parcVelo = (JSONObject) root.get(i);
-                int number = (int) parcVelo.get("number");
-                String name = (String) parcVelo.get("name");
-                //tvHello.append(name.toString());
-                String adress = (String) parcVelo.get("adress");
-                double latitude = (double) parcVelo.get("latitude");
-                double longitude = (double) parcVelo.get("longitude");
+        stations = extractStation(MainActivity.this);
 
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
 
     }
