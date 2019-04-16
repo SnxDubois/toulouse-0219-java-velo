e644229a0e367b35e7fa334de0a78cac84cab3d2
Joséphine Godard <josephine.godard@gmail.com>
2019-04-16 18:02:28 +0200

    Modification google

--- app/src/main/java/fr/wildcodeschool/metro/SplashActivity.java
+++ app/src/main/java/fr/wildcodeschool/metro/SplashActivity.java
@@ -2,11 +2,13 @@ package fr.wildcodeschool.metro;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -25,11 +27,11 @@ import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SplashActivity extends AppCompatActivity {
 
-    GoogleSignInClient mGoogleSignInClient;
     private SignInButton signIn;
-    private int RC_SIGN_IN = 1;
-    private String TAG = "SplachActivity";
+    private int RC_SIGN_IN=1;
+    private String TAG = "SplashActivity";
     private FirebaseAuth mAuth;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,14 +54,14 @@ public class SplashActivity extends AppCompatActivity {
         };
         myThread.start();
 
-        signIn = (SignInButton) findViewById(R.id.sign_in_button);
+        signIn = (SignInButton)findViewById(R.id.sign_in_button);
         mAuth = FirebaseAuth.getInstance();
 
         // Configure sign-in to request the user's ID, email address, and basic
 // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestIdToken("89051023672-l62ct47flok6qc8rjidc5297phm3j5p8.apps.googleusercontent.com")
                 .requestEmail()
                 .build();
 
@@ -115,7 +117,7 @@ public class SplashActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(SplashActivity.this, "you are no able to log in to google", Toast.LENGTH_LONG).show();
+                            Toast.makeText(SplashActivity.this,"you are no able to log in to google",Toast.LENGTH_LONG).show();
                             updateUI(null);
                         }
 
