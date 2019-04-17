c20b325ae741be31b63429a7db7e045c64e3d1f8
cedric <casas.cedric@gmail.com>
2019-04-17 14:05:19 +0200

    ajout des vélos et des bornes disponibles dans l'info window des marqueurs

--- app/src/main/java/fr/wildcodeschool/metro/CustomInfoWindowAdapter.java
+++ app/src/main/java/fr/wildcodeschool/metro/CustomInfoWindowAdapter.java
@@ -20,7 +20,9 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     }
 
     private void rendowWindowText(Marker marker, View view) {
+        //récupération de l'objet marker
         Station recupMarker = (Station) marker.getTag();
+
         String title = marker.getTitle();
         TextView tvTitle = (TextView) view.findViewById(R.id.tvTitle);
 
@@ -33,6 +35,20 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
         if (!snippet.equals("")) {
             tvSnippet.setText(snippet);
+        }
+
+        int bikeAvailable = recupMarker.getAvailableBikes();
+        TextView tvBikeAvailable = (TextView) view.findViewById(R.id.tvAvailableBike);
+
+        if (bikeAvailable != 0) {
+            tvBikeAvailable.setText(String.valueOf(bikeAvailable) + " " + mContext.getString(R.string.velos_disponibles));
+        }
+
+        int standsAvailable = recupMarker.getAvailableStands();
+        TextView tvStandsAvailable = (TextView) view.findViewById(R.id.tvAvailableStands);
+
+        if (standsAvailable != 0) {
+            tvStandsAvailable.setText(String.valueOf(standsAvailable) + " " + mContext.getString(R.string.places_disponibles));
         }
     }
 
