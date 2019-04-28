01f3dd988a3bb0053a01c34532fcadbfaeee74b4
Anaclet Biquet <anaclet.biquet@gmail.com>
2019-04-28 19:35:00 +0200

    erase data

--- app/src/main/java/fr/wildcodeschool/metro/StationsRecyclerAdapter.java
+++ app/src/main/java/fr/wildcodeschool/metro/StationsRecyclerAdapter.java
@@ -1,6 +1,6 @@
 package fr.wildcodeschool.metro;
 
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,13 +10,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-
-import static fr.wildcodeschool.metro.Helper.extractStation;
 
 public class StationsRecyclerAdapter extends RecyclerView.Adapter<StationsRecyclerAdapter.ViewHolder> {
 
@@ -68,30 +67,24 @@ public class StationsRecyclerAdapter extends RecyclerView.Adapter<StationsRecycl
                     holder.favoriteView.setImageResource(R.drawable.ic_favorite_unchecked);
                     holder.favoriteView.setTag(R.drawable.ic_favorite_unchecked);
 
-
-
-
-
-                    favoriteStationBase.addListenerForSingleValueEvent(new ValueEventListener() {
+                    favoriteStationBase.orderByKey().equalTo(mStations.get(position).getNumber()).addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(DataSnapshot dataSnapshot) {
-
-                                }
-                            });
-                        }
-
-
-
-
-
-
-
-
+                            for (DataSnapshot postsnapshot :dataSnapshot.getChildren()) {
+                                String key = postsnapshot.getKey();
+                                dataSnapshot.getRef().removeValue();
+                            }
 
                 }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
                 else {
                     holder.favoriteView.setImageResource(R.drawable.ic_favorite_checked);
                     holder.favoriteView.setTag(R.drawable.ic_favorite_checked);
+
                     String key = Integer.toString(mStations.get(position).getNumber());
                     favoriteStationBase.child(key).setValue(mStations.get(position).getNumber());
                 }
